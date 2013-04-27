9495095919841ac825863ce4ab037c52bf97a2b5
Andrew Zborovskyi <zbrowskiy@gmail.com>
2013-04-27 11:35:58 +0300

    - changes

--- src/jenkinsTest/HelloWorld.java
+++ src/jenkinsTest/HelloWorld.java
@@ -3,6 +3,5 @@ package jenkinsTest;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("One more line"); 
     }
 }
