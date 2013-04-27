f146218b3e9c73c79049b3f28e6b9869753fab4f
Andrew Zborovskyi <zbrowskiy@gmail.com>
2013-04-27 17:40:53 +0300

    - largest implemented

--- src/main/java/org/maventest/App.java
+++ src/main/java/org/maventest/App.java
@@ -10,4 +10,20 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    public static int largest(int[] list) {
+        int index, max=Integer.MAX_VALUE;
+
+        if (list.length == 0) {
+            throw new RuntimeException("Empty list");
+        }
+
+
+        for (index = 0; index < list.length-1; index++) {
+            if (list[index] > max) {
+                max = list[index];
+            }
+        }
+        return max;
+    }
 }
--- src/test/java/org/maventest/AppTest.java
+++ src/test/java/org/maventest/AppTest.java
@@ -35,4 +35,28 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+
+    public void testOrder() {
+        assertEquals(9, App.largest(new int[] {9,8,7}));
+        assertEquals(9, App.largest(new int[] {8,9,7}));
+        assertEquals(9, App.largest(new int[] {7,8,9}));
+    }
+
+    public void testEmpty() {
+        try {
+            App.largest(new int[] {});
+            fail("Should have thrown an exception");
+        } catch (RuntimeException e) {
+            assertTrue(true);
+        }
+    }
+
+    public void testOrder2() {
+        int[] arr = new int[3];
+        arr[0] = 8;
+        arr[1] = 9;
+        arr[2] = 7;
+        assertEquals(9, App.largest(arr));
+    }
 }
