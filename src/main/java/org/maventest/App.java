ed967ce8ed261325a24ab4358bf99ccc11498b0b
Andrew Zborovskyi <zbrowskiy@gmail.com>
2013-04-27 17:45:53 +0300

    - fixed algorithm

--- src/main/java/org/maventest/App.java
+++ src/main/java/org/maventest/App.java
@@ -12,14 +12,14 @@ public class App
     }
 
     public static int largest(int[] list) {
-        int index, max=Integer.MAX_VALUE;
+        int index, max=0;
 
         if (list.length == 0) {
             throw new RuntimeException("Empty list");
         }
 
 
-        for (index = 0; index < list.length-1; index++) {
+        for (index = 0; index < list.length; index++) {
             if (list[index] > max) {
                 max = list[index];
             }
